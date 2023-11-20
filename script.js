3ee3f70fb480bc42003ab2348d3697336d9864c1
alienshy <alisahverdiyew8@gmail.com>
2023-11-21 01:38:56 +0400

    commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let addPerson = () => {
-    let adsoyad = document.getElementById("adsoyad").value;
+    let ad = document.getElementById("ad").value;
+    let soyad = document.getElementById("soyad").value;
     let yas = document.getElementById("yas").value;
 
-    if (adsoyad && yas) {
+    if (ad && soyad && yas) {
         let table = document.getElementById("adyasalt");
         let row = table.insertRow(1);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
-        cell1.innerHTML = adsoyad;
+        cell1.innerHTML = ad + " " + soyad;
         cell2.innerHTML = yas;
         document.getElementById("adsoyaducun").reset();
     } else {
-        alert("xahis olunur melumati daxil edin");
+        alert("Xahiş olunur, məlumatları daxil edin.");
     }
 };
+
